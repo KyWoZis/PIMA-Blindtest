a1e6c6a56ddc28b4e45859a59b629bdba54ba34a
saes-vin2022 <mickael.saes-vincensini@ensiie.eu>
2023-10-15 21:53:17 +0200

    j'ai un truc qui marche un peu, je lit une vidéo, tjr buggé atm

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,17 @@
 // Création d'une liste de vidéos
 const videoList = [
+    "0.mkv",
     "1.mkv",
-    "2.mkv",
 ];
 
 // Variable de chemin commun
-const videoPath = "./vid/";
+const videoPath = "vid/";
 
 // Fonction qui lit une vidéo
 function playVideo() {
     const videoElement = document.getElementById("video-player");
-    videoElement.src = videoPath + videoList[1];
+    videoElement.style.display = "block"; // Afficher la vidéo lorsque le bouton "Play" est cliqué
+    videoElement.src = videoPath + videoList[0];
     videoElement.play();
 }
 
@@ -18,6 +19,7 @@ function playVideo() {
 function nextVideo() {
     const videoElement = document.getElementById("video-player");
     const currentVideoIndex = videoList.indexOf(videoElement.src.replace(videoPath, ""));
+    console.log((currentVideoIndex+1)% videoList.length);
     const nextVideoIndex = (currentVideoIndex + 1) % videoList.length;
     videoElement.style.display = "none";
     playVideo(nextVideoIndex);
