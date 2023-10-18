87cadb2344b461a9730db42275841bc0b057dbfe
saes-vin2022 <mickael.saes-vincensini@ensiie.eu>
2023-10-18 22:44:42 +0200

    ajout de commentaires temporaires

--- js/functions.js
+++ js/functions.js
@@ -10,22 +10,28 @@ const videoList = [
 
 // Fonction qui démarre la première vidéo et cache l'élément vidéo
 function beginBT() {
-    const videoElement = document.getElementById("video-player");
-    videoElement.src = videoPath + videoList[0];
+    const videoElement = document.getElementById("video-player"); // Récupère l'élément vidéo
+    videoElement.src = videoPath + videoList[0]; // Définit la source de la vidéo
     videoElement.style.display = "block"; // Affiche l'élément vidéo
-    videoElement.play();
-    console.log(videoElement.src)
+    videoElement.play(); // Démarre la vidéo
+    console.log(videoElement.src) 
+    videoElement.addEventListener('pause', function() {
+        videoElement.play(); // Empêche la pause
+    });
 }
 
 
 function nextVideo() {
-    const videoElement = document.getElementById("video-player");
-    const currentVideoIndex = videoList.indexOf(videoElement.src.replace(videoPath, ""));
-    console.log(currentVideoIndex)
-    const nextVideoIndex = (currentVideoIndex + 1) % videoList.length;
+    const videoElement = document.getElementById("video-player"); // Récupère l'élément vidéo
+    const currentVideoIndex = videoList.indexOf(videoElement.src.replace(videoPath, "")); // Récupère l'index de la vidéo actuelle
+    console.log(currentVideoIndex) 
+    const nextVideoIndex = (currentVideoIndex + 1) % videoList.length; // Récupère l'index de la prochaine vidéo
     console.log(nextVideoIndex)
-    videoElement.style.display = "block";
-    videoElement.src = videoPath + videoList[nextVideoIndex];
-    console.log(videoElement.src)
-    videoElement.play();
+    videoElement.style.display = "block"; // Affiche l'élément vidéo
+    videoElement.src = videoPath + videoList[nextVideoIndex]; // Définit la source de la vidéo
+    console.log(videoElement.src) 
+    videoElement.play(); // Démarre la vidéo
+    videoElement.addEventListener('pause', function() {
+        videoElement.play(); // Empêche la pause
+    });
 }
