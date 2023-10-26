618272dca5da14732597f9514c75bc87fb8efea0
saes-vin2022 <mickael.saes-vincensini@ensiie.eu>
2023-10-26 15:03:43 +0200

    Mise en forme du code

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
-// const mysql = require('mysql2');
+//Import 
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
+
 // create a connection to the database
 const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
@@ -20,7 +21,7 @@ connection.connect((err) => {
     console.log('connected as id ' + connection.threadId);
 });
 
-
+//functions 
 async function getUser() {
     const [rows] = await connection.query("SELECT * FROM test");
     return rows;
@@ -28,5 +29,6 @@ async function getUser() {
 const result = await getUser();
 const rows = result[0];
 console.log(rows);
+
 // close the connection
 connection.end();
