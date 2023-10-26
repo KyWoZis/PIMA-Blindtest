7c77f6b622220f5f020dc3ed4b2f957a6874156c
saes-vin2022 <mickael.saes-vincensini@ensiie.eu>
2023-10-26 14:52:04 +0200

    en concordant avec le .envça marche mieux^^

--- database.js
+++ database.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 // create a connection to the database
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }).promise();
 
 // connect to the database
@@ -22,7 +22,7 @@ connection.connect((err) => {
 
 
 async function getUser() {
-    const [rows] = await connection.execute('SELECT * FROM test');
+    const [rows] = await connection.query('SELECT * FROM test');
     return rows;
 }
 const result = getUser();
