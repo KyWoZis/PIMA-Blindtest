b9e15866dbb3c4da05ad9e44b5b77826c58a19a9
hugo.lallemant <hugo.lallemant@ensiie.eu>
2023-11-01 14:21:45 +0100

    post request create user

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import {getUsers, getUserById, createUser} from './database.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
@@ -11,6 +13,12 @@ app.use((err, req, res, next) => {
 app.get('/users', async (req, res) => {
     const users = await getUsers();
     res.send(users);
+})
+
+app.post('/createuser', async (req, res) => {
+    const {username, password} = req.body;
+    await createUser(username, password);
+    res.send('User created');
 })
 
 app.listen(3000, () => {
