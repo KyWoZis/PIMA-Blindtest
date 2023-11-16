af70c1b51fbce8a2310a036cec0b7075f242d674
hugo.lallemant <hugo.lallemant@ensiie.eu>
2023-11-16 09:24:05 +0100

    repaired playlist

--- routes/playlist.js
+++ routes/playlist.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import multer from 'multer';
-import fs from 'fs';
 import {addMusicToPlaylist, removeMusicFromPlaylist, createPlaylist, removePlaylist, getPlaylists, getAllPlaylists, getSongsFromPlaylist, playlistExists} from '../database.js';
 import { create } from 'domain';
 var router = express.Router();
@@ -17,7 +15,7 @@ router.get('/add', function(req, res, next) {
     res.render('createPlaylist', {title: "Add a playlist"});
 });
 
-router.post('/add', multer().any() ,async (req, res) => {
+router.post('/add', async (req, res) => {
     const {user_id, playlistName} = req.body;
 
     if (await playlistExists(user_id, playlistName)) {
