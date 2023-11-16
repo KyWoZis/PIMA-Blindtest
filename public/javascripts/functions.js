ea6a8a66967b5b2046e668eec7e2e47b70be12ee
root <root@fujitsu-mega.home>
2023-11-16 02:00:12 +0100

    passage a une autre musique / transition / un peu d'ui fait ^^

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -4,87 +4,106 @@ const videoPath = "../videos/";
 
 // Création d'une liste de vidéos
 const videoList = [
-    "0.mkv",
-    "1.mkv",
-    "2.mkv",
-];
+    "0.mp4",
+    "1.mp4",
+    "2.mp4",
+]; // a changer grace au nouveau systeme
 
 // Création d'une liste de noms associés aux vidéos
 const nameList = [
     "Lain",
     "Usseewa",
     "One Piece",
-];
+]; // same 
 
+delay = 2000;
 index = 0;
 score = 0;
 
+videoElement=null;
+
+startSection=null;
+playSection=null;
+transitionSection=null;
+endSection=null;
+videoSection=null;
+
 // Fonction qui démarre la première vidéo et cache l'élément vidéo
 function beginBT() {
-    const videoElement = document.getElementById("video-player"); // Récupère l'élément vidéo
-    videoElement.src = videoPath + videoList[0]; // Définit la source de la vidéo
-    videoElement.style.display = "block"; // Affiche l'élément vidéo
-    videoElement.play(); // Démarre la vidéo
-    console.log(videoElement.src) 
-    videoElement.addEventListener('pause', function() {
-        videoElement.play(); // Empêche la pause
-    });
+    videoElement = document.getElementById("video-player"); // Récupère l'élément vidéo
+    startSection = document.getElementById("start");
+    playSection = document.getElementById("play");
+    transitionSection = document.getElementById("transition");
+    endSection = document.getElementById("end");
+    videoSection = document.getElementById("video");
 
-    const playButton = document.getElementById("playButton");   // Récupère l'élément bouton play
-    playButton.style.display = "none"; // Cache le bouton play
 
-      // Afficher la zone de texte
-      const textInput = document.getElementById("textInput");
-      textInput.style.display = "block";
-      textInput.value = ''; // Réinitialiser le texte à chaque fois
-    
-    // Afficher le bouton de soumission
-    const submitButton = document.getElementById("submitButton");
-    submitButton.style.display = "block";
-
-    // Afficher le bouton "Next"
-    const nextButton = document.getElementById("nextButton");
-    nextButton.style.display = "block";
-
-    //à termes il faudra que l'on encapsule nos fonctions pour ne plus avoir à toucher ici
+    startSection.style.display = "none"; // Cache la section start
+    videoSection.style.display = "block";
+    playSection.style.display = "block";
+    song()
 }
 
+function song(){
+    videoElement.src = videoPath + videoList[index]; // Définit la source de la vidéo
+    videoElement.play(); // Démarre la vidéo
 
-function nextButton() {
-    if (index < videoList.length - 1) {
-        nextVideo();
-    }
-    else {
-        endBT();
-    }
+    // Réinitialiser la zone de texte
+    const textInput = document.getElementById("textInput");
+    textInput.style.display = "block";
+    textInput.value = '';
+    setTimeout(() => {
+        transition();
+      }, delay);
+}
+
+function transition(){
+
+    const hideDiv = document.getElementById("hide");
+    hideDiv.style.display  = "none";
+    playSection.style.display = "none";
+    transitionSection.style.display = "block";
+    videoElement.src = videoPath + videoList[index]; // Définit la source de la vidéo
+    videoElement.play(); // Démarre la vidéo
+    
+    // good answer 
+    const currentAnswer = textInput.value.trim().toLowerCase();
+    const associatedName = nameList[index].toLowerCase();
+    // update labels
+    const answerLabel = document.getElementById("answer"); 
+    answerLabel.innerHTML = associatedName;
+    const correctionLabel = document.getElementById("correction"); 
+    correctionLabel.innerHTML = associatedName==currentAnswer?"Yup !!":"Nope but well tried !";
+    
+    index = index + 1;
+    //next song
+    setTimeout(() => {
+        if(index >= videoList.length - 1)
+            endBT()
+        else{
+            hideDiv.style.display  = "block";
+            playSection.style.display = "block";
+            transitionSection.style.display = "none";
+            song()
+        }
+      }, delay);
+
 }
 
 function endBT() {
-    alert("Fin du jeu ! Votre score est de " + score + " points.");
+    videoSection.style.display = "none";
+    transitionSection.style.display = "none";
+    endSection.style.display = "block";
+
 }
 
-
+/*
 function nextVideo() {
-    const videoElement = document.getElementById("video-player"); // Récupère l'élément vidéo
-    console.log(index) 
-    index = (index + 1)  % videoList.length
-    console.log(index)
-    videoElement.style.display = "block"; // Affiche l'élément vidéo
+
     videoElement.src = videoPath + videoList[index]; // Définit la source de la vidéo
     console.log(videoElement.src) 
     videoElement.play(); // Démarre la vidéo
-    videoElement.addEventListener('pause', function() {
-        videoElement.play(); // Empêche la pause
-    });
 
-     // Réinitialiser la zone de texte
-     const textInput = document.getElementById("textInput");
-     textInput.style.display = "block";
-     textInput.value = '';
-
-     // Afficher le bouton de soumission
-    const submitButton = document.getElementById("submitButton");
-    submitButton.style.display = "block";
 }
 
 // Fonction pour vérifier la réponse du texte
@@ -100,3 +119,4 @@ function checkAnswer() {
         alert("Mauvaise réponse !");
     }
 }
+*/
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{pug,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
