085805e546ecfecc2e9cbc8ae6f011b725fb1234
hugo.lallemant <hugo.lallemant@ensiie.eu>
2023-11-17 17:17:15 +0100

    javascript function to update music table with search parameter

---
+++ public/javascripts/updateTable.js
@@ -0,0 +1,41 @@
+function updateTable(param, db) {
+    const tableBody = document.getElementById('tableBody');
+
+    //Get data from db json where a a value contains param
+    const data = db.filter(item => {
+        return Object.keys(item).some(key => {
+            return item[key].toString().toLowerCase().includes(param.toLowerCase());
+        });
+    });
+    
+    tableBody.innerHTML = ''; // Clear the existing table body content
+    data.forEach(result => {
+        // Create a new row for each search result
+        const row = document.createElement('tr');
+        // Create table cells for each data field
+        Object.keys(result).forEach(key => {
+        const cell = document.createElement('td');
+        cell.textContent = result[key];
+        row.appendChild(cell);
+        });
+        // Create a cell for the "Delete" button
+        const deleteCell = document.createElement('td');
+        const deleteForm = document.createElement('form');
+        deleteForm.action = 'delete';
+        deleteForm.method = 'get';
+        const deleteInput = document.createElement('input');
+        deleteInput.type = 'hidden';
+        deleteInput.name = 'id';
+        deleteInput.value = result.music_id;
+        const deleteButton = document.createElement('button');
+        deleteButton.type = 'submit';
+        deleteButton.textContent = 'Delete';
+        deleteButton.id = 'deleteButton';
+        deleteForm.appendChild(deleteInput);
+        deleteForm.appendChild(deleteButton);
+        deleteCell.appendChild(deleteForm);
+        row.appendChild(deleteCell);
+        // Add the row to the table body
+        tableBody.appendChild(row);
+    });
+};
