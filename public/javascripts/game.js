91af5c108c4ced8f215ce8edfda1e61740ffc0b3
surenzo <enzo.mariant@ensiie.eu>
2023-11-28 22:54:54 +0100

    display all required field for the blindtest (name, artiste &so on)
    it hides if the fileds are null / empty

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -11,24 +11,32 @@ const naame = [
 ]; // same 
 list = [];
 nameList = [];
+ArtistList = [];
+OriginList = [];
+MusicTypeList = [];
 function dataToListMusic(musics,infoMusics){
     musics.forEach(music =>
     {
         list.push(music.music_id)
-        console.log(list)
-        console.log(infoMusics)
         const name = infoMusics.filter(music1 => music1.music_id === music.music_id);
-        if(name.length>0) console.log(name[0].music_name);
         nameList.push(name[0].music_name);
+        ArtistList.push(name[0].artist_name);
+        OriginList.push(name[0].origin);
+        MusicTypeList.push(name[0].music_type);
     });// a changer quand je serais plus reveillé : pas besoin des musics si on a infomusics ( a part peut etre pour avoir leur ordre)
 }
 
-delay = 2000;
+delay = 20000;
 index = 0;
 score = 0;
+//elements de réponses
 
+Music_name = null;
+Artist_name = null;
+Origin = null;
+Music_type = null;
 videoElement=null;
-
+//sections
 startSection=null;
 playSection=null;
 transitionSection=null;
@@ -38,13 +46,18 @@ videoSection=null;
 // Fonction qui démarre la première vidéo et cache l'élément vidéo
 function beginBT() {
     dataToListMusic(data,infoMusics);
+    //recupere les sections
     videoElement = document.getElementById("video-player"); // Récupère l'élément vidéo
     startSection = document.getElementById("start");
     playSection = document.getElementById("play");
     transitionSection = document.getElementById("transition");
     endSection = document.getElementById("end");
     videoSection = document.getElementById("video");
-
+    //recupere les elements de reponses
+    Music_name = document.getElementById("textInputMusic_name");
+    Artist_name = document.getElementById("textInputArtist");
+    Origin = document.getElementById("textInputOrigin");
+    Music_type = document.getElementById("textInputMusic_Type");
 
     startSection.style.display = "none"; // Cache la section start
     videoSection.style.display = "block";
@@ -57,9 +70,34 @@ function song(){
     videoElement.play(); // Démarre la vidéo
 
     // Réinitialiser la zone de texte
-    const textInput = document.getElementById("textInput");
-    textInput.style.display = "block";
-    textInput.value = '';
+    // faire ca dans une fonction non :eyes: ?
+    if (nameList[index] === null) {
+        Music_name.style.display = 'none';
+    } else {
+        Music_name.value = '';
+        Music_name.style.display = 'block';
+    }
+
+    if (ArtistList[index] === null) {
+        Artist_name.style.display = 'none';
+    } else {
+        Artist_name.value = '';
+        Artist_name.style.display = 'block';
+    }
+
+    if (OriginList[index] === null) {
+        Origin.style.display = 'none';
+    } else {
+        Origin.value = '';
+        Origin.style.display = 'block';
+    }
+
+    if (MusicTypeList[index] === null) {
+        Music_type.style.display = 'none';
+    } else {
+        Music_type.value = '';
+        Music_type.style.display = 'block';
+    }
     setTimeout(() => {
         transition();
       }, delay);
@@ -73,16 +111,13 @@ function transition(){
     transitionSection.style.display = "block";
     videoElement.src = videoPath + list[index]+".mp4"; // Définit la source de la vidéo
     videoElement.play(); // Démarre la vidéo
-    
-    // good answer 
-    const currentAnswer = textInput.value.trim().toLowerCase();
-    const associatedName = nameList[index].toLowerCase();
-    // update labels
-    const answerLabel = document.getElementById("answer"); 
-    answerLabel.innerHTML = associatedName;
-    const correctionLabel = document.getElementById("correction"); 
-    correctionLabel.innerHTML = associatedName==currentAnswer?"Yup !!":"Nope but well tried !";
-    
+
+
+    // Utilisation de la fonction pour chaque ensemble de valeurs
+    updateLabels(Music_name, nameList[index], "answerMusicName", "correctionMusicName");
+    updateLabels(Artist_name, ArtistList[index], "answerArtistName", "correctionArtistName");
+    updateLabels(Origin, OriginList[index], "answerOrigin", "correctionOrigin");
+    updateLabels(Music_type, MusicTypeList[index], "answerMusicType", "correctionMusicType");
     index = index + 1;
     //next song
     setTimeout(() => {
@@ -96,6 +131,24 @@ function transition(){
         }
       }, delay);
 
+}
+
+function updateLabels(inputElement, associatedValue, answerLabelId, correctionLabelId) {
+    const currentAnswer = inputElement.value.trim().toLowerCase();
+    const associated = associatedValue.toLowerCase();
+
+    const answerLabel = document.getElementById(answerLabelId);
+    const correctionLabel = document.getElementById(correctionLabelId);
+
+    if (associatedValue === "") {
+        answerLabel.style.display = 'none';
+        correctionLabel.style.display = 'none';
+    } else {
+        answerLabel.innerHTML = associated;
+        correctionLabel.innerHTML = associated === currentAnswer ? "Yup !!" : "Nope but well tried !";
+        answerLabel.style.display = 'block';
+        correctionLabel.style.display = 'block';
+    }
 }
 
 function endBT() {
